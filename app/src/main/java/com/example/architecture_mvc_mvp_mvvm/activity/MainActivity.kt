00e12872679b3dbf94554952790f9a6188849aba
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-09-06 17:28:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/architecture_mvc_mvp_mvvm/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.architecture_mvc_mvp_mvvm.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.architecture_mvc_mvp_mvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
